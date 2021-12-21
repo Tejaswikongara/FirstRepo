8631eb1d5730937da6dac1862598db12fd190944
Tejaswikongara <96280094+Tejaswikongara@users.noreply.github.com>
2021-12-21 13:08:14 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+
+public class HelloWorld
+{
+  public static void main(String[] args) {
+    System.out.println("Hello World from Jenkins!!!");
+    System.out.println("Hello World from Jenkins!!!");
+    
+  }
+}
+    
